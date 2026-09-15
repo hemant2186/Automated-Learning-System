6c61a2ce592a485fb6379deeb4e031847f5ce2ac
Hemant Kumar <hemantkumar90089h@gmail.com>
2026-09-15 22:50:23 +0530

    Expose career resources through resources API

--- backend/routes/resources.js
+++ backend/routes/resources.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const resourceController = require('../controllers/resourceController');
+const careerResourceController = require('../controllers/careerResourceController');
 
 const router = express.Router();
 
+router.get('/career/:careerSlug', careerResourceController.list);
 router.get('/path/:pathSlug', resourceController.listResourcesByPath);
 router.get('/', resourceController.listResources);
 
