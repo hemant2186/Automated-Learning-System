e71f2f96ed6875ab250a28d604434083b3f5df66
Hemant Kumar <hemantkumar90089h@gmail.com>
2026-09-15 23:26:38 +0530

    Expose career readiness endpoint

--- backend/routes/careers.js
+++ backend/routes/careers.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/careerController');
 const planController = require('../controllers/careerPlanController');
 const journeyController = require('../controllers/careerJourneyController');
 const proofController = require('../controllers/careerProofController');
+const readinessController = require('../controllers/careerReadinessController');
 const resourceController = require('../controllers/careerResourceController');
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.get('/me/plan', auth, planController.getPlan);
 router.post('/me/plan', auth, planController.savePlan);
 router.get('/me/journey', auth, journeyController.getJourney);
 router.post('/me/journey/:skillKey/:stage/complete', auth, journeyController.completeStage);
+router.get('/me/readiness', auth, readinessController.getReadiness);
 router.post('/me/proof', auth, proofController.createProof);
 router.get('/:slug/resources', resourceController.list);
 router.get('/:slug', controller.getCareer);
